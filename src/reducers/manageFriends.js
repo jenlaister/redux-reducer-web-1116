e39e8c5a3ccb53422c19004d6a113438423e98e7
Jenny Lai <jenny.lai@flatironschool.com>
2017-02-06 17:05:54 -0500

    Done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,10 @@
 export function manageFriends(state, action){
+  if(action.type === 'ADD_FRIEND') {
+    return {...state, friends: [ ...state.friends, action.payload]}
+  } else if(action.type === 'REMOVE_FRIEND') {
+    let remove = state.friends.findIndex(friend => friend.id === action.payload)
+    return {...state, friends: [...state.friends.slice(0, remove), ...state.friends.slice(remove + 1)]}
+  } else {
+    return state
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,7 @@
 export function managePresents(state, action){
+  if(action.type === 'INCREASE') {
+    return { numberOfPresents: +1 }
+  } else {
+    return state
+  }
 }
